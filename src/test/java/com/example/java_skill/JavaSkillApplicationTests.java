888c874b835dfb31af15d7a71921cbda8c7f15f2
fuqiang <18251359382@163.com>
2019-12-23 21:44:37 +0800

    commit

--- src/test/java/com/example/java_skill/JavaSkillApplicationTests.java
+++ src/test/java/com/example/java_skill/JavaSkillApplicationTests.java
@@ -5,9 +5,7 @@ import com.example.java_skill.jdk8.CartService;
 import com.example.java_skill.jdk8.Sku;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
