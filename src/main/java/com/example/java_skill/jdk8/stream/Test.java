ad8574eefe882ce7bdfb03e6acddf618fc5fd062
fuqiang <18251359382@163.com>
2019-12-28 23:32:52 +0800

    stream test

--- src/main/java/com/example/java_skill/jdk8/stream/Test.java
+++ src/main/java/com/example/java_skill/jdk8/stream/Test.java
@@ -9,6 +9,7 @@ import com.example.java_skill.jdk8.CartService;
 import com.example.java_skill.jdk8.Sku;
 import com.example.java_skill.jdk8.SkuCategoryEnum;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
@@ -19,6 +20,8 @@ import java.util.stream.Collectors;
  * @version Test, v0.1 2019-12-28 22:49
  */
 public class Test {
+
+    public static List<Sku> cartSkuList = CartService.getCartSkuList();
 
     public static void test() {
         AtomicReference<Double> money = new AtomicReference<>(0.0);
@@ -35,9 +38,50 @@ public class Test {
         System.out.println(money);
     }
 
-    public static void main(String[] args) {
+    /**
+     * map:将一个元素转换为另一个元素
+     */
+    public static void mapTest() {
+        List<String> nameList = cartSkuList.stream().map(sku -> sku.getSkuName()).collect(Collectors.toList());
+    }
 
-        test();
+    /**
+     * flatMap:将一个对象转换为流
+     */
+    public static void flatMapTest() {
+        cartSkuList.stream().flatMap(sku -> Arrays.stream(sku.getSkuName().split("")))
+                .forEach(e -> System.out.println(JSON.toJSONString(e, true)));
+    }
+
+    /**
+     * peek:无状态的中间操作
+     */
+    public static void peekTest() {
+        cartSkuList.stream().peek(sku -> System.out.println(sku.getSkuName()))
+                .forEach(e -> System.out.println(JSON.toJSONString(e, true)));
+    }
+
+    /**
+     * sorted:有状态的中间操作
+     */
+    public static void sortedTest() {
+        cartSkuList.stream().peek(sku -> System.out.println(sku.getSkuName()))
+                .sorted(Comparator.comparing(Sku::getTotalPrice).reversed())
+                .forEach(e -> System.out.println(JSON.toJSONString(e, true)));
+    }
+
+    /**
+     * distinct:有状态的中间操作
+     */
+    public static void distinctTest() {
+        cartSkuList.stream().map(sku -> sku.getSkuCategory())
+                .distinct()
+                .forEach(e -> System.out.println(e));
+    }
+
+
+    public static void main(String[] args) {
+        distinctTest();
     }
 
 }
